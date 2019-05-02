c9ee1f3901dc8471ef8d276ce05379854c2d70a6
Albert <titovalbert@yandex.ru>
2019-05-02 14:17:50 +0300

    ProjectResources, ActivitySplash and ActivityHome

---
+++ app/src/main/java/newfarmstudio/ru/activities/ActivityHome.kt
@@ -0,0 +1,73 @@
+package newfarmstudio.ru.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.widget.FrameLayout
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import newfarmstudio.ru.R
+
+class ActivityHome : AppCompatActivity() {
+
+    private lateinit var drawer: Drawer
+    private lateinit var toolbar: Toolbar
+
+    private var channelNames: Array<String>? = null
+    private var channelId: Array<String>? = null
+    private var videoTypes: Array<String>? = null
+
+    private var selectedDrawerItem: Int? = null
+
+    private lateinit var layoutList: FrameLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        layoutList = findViewById(R.id.fragment_container)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        channelNames = resources.getStringArray(R.array.channel_names)
+        channelId = resources.getStringArray(R.array.channel_id)
+        videoTypes = resources.getStringArray(R.array.video_types)
+
+        val primaryDrawerItems: ArrayList<IDrawerItem<*>> = arrayListOf()
+
+        for (i in 0 until channelId!!.size - 1) {
+            val name = channelNames!![i]
+            primaryDrawerItems.add(PrimaryDrawerItem()
+                .withName(name)
+                .withIdentifier(i)
+                .withSelectable(false))
+        }
+
+        val accountHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .build()
+
+        drawer = DrawerBuilder(this)
+            .withActivity(this)
+            .withAccountHeader(accountHeader)
+            .withDisplayBelowStatusBar(true)
+            .withActionBarDrawerToggleAnimated(true)
+            .withSavedInstance(savedInstanceState)
+            .withDrawerItems(primaryDrawerItems)
+            .addStickyDrawerItems(
+                SecondaryDrawerItem()
+                    .withName(getString(R.string.about))
+                    .withIdentifier(channelId!!.size - 1)
+                    .withSelectable(false)
+            )
+            .withOnDrawerNavigationListener {
+                return@withOnDrawerNavigationListener false
+            }
+            .withSavedInstance(savedInstanceState)
+            .withShowDrawerOnFirstLaunch(true)
+            .build()
+    }
+}
---
+++ app/src/main/java/newfarmstudio/ru/activities/ActivitySplash.kt
@@ -0,0 +1,60 @@
+package newfarmstudio.ru.activities
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import newfarmstudio.ru.R
+
+class ActivitySplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Loading(this).execute()
+    }
+
+    class Loading(context: Context) : AsyncTask<Unit, Unit, Unit>() {
+
+        @SuppressLint("StaticFieldLeak")
+        var mContext: Context = context.applicationContext
+
+        override fun doInBackground(vararg params: Unit?) {
+            try {
+                Thread.sleep(2000)
+            }
+            catch(ie: InterruptedException) {
+                ie.printStackTrace()
+            }
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+            val intent = Intent(mContext, ActivityHome::class.java)
+            mContext.startActivity(intent)
+        }
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (hasFocus) {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    .or(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+                    .or(View.SYSTEM_UI_FLAG_FULLSCREEN)
+                    .or(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            }
+        }
+    }
+}
